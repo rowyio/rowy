a2435c35ffd55276ad1865375650eab3edcb4384
shams mosowi <shams.mosowi@gmail.com>
2019-09-12 18:16:36 +1000

    create new rows from table view

--- src/views/TableView.tsx
+++ src/views/TableView.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-import { Navigation } from "../components/Navigation";
+import Navigation from "../components/Navigation";
 import useTable from "../hooks/useTable";
 import Table from "../components/Table";
 import useRouter from "../hooks/useRouter";
 import useTableConfig from "../hooks/useTableConfig";
+import Button from "@material-ui/core/Button";
 const useStyles = makeStyles({});
 
 export default function AuthView() {
@@ -29,6 +30,7 @@ export default function AuthView() {
         addColumn={configActions.addColumn}
         tableActions={tableActions}
       />
+      <Button onClick={tableActions.addRow}>Add Row</Button>
     </Navigation>
   );
 }
