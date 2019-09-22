018c644b6fb14d273762a1bc99190686daabb743
Sidney Alcantara <sidney@sidney.me>
2019-09-22 22:48:35 +1000

    move MUI theme to own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+
 import { ThemeProvider } from "@material-ui/styles";
-import { createMuiTheme } from "@material-ui/core";
+import Theme from "./Theme";
 
 import AuthView from "./views/AuthView";
 import TableView from "./views/TableView";
@@ -9,20 +10,11 @@ import TablesView from "./views/TablesView";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AuthProvider } from "./AuthProvider";
 import CustomBrowserRouter from "./util/CustomBrowserRouter";
-
 import PrivateRoute from "./util/PrivateRoute";
-const theme = createMuiTheme({
-  spacing: 4,
-  palette: {
-    primary: {
-      main: "#007bff"
-    }
-  }
-});
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={Theme}>
       <AuthProvider>
         <CustomBrowserRouter>
           <div>
