a108763a9ed81b062c230fa0bf0d8cd1347c5b78
shams mosowi <shams.mosowi@gmail.com>
2019-10-28 11:59:41 +1100

    updated user->founder mapping

--- cloud_functions/functions/src/maps.ts
+++ cloud_functions/functions/src/maps.ts
@@ -3,21 +3,10 @@ const admin = require("firebase-admin");
 const db = admin.firestore();
 export const users = functions.firestore
   .document("users/{id}")
-  .onUpdate(async (change, context) => {
+  .onUpdate(async(change, context) => {
     const afterData = change.after.data();
 
-    if (afterData && afterData.founder[0].docPath)
-      await db
-        .doc(afterData.founder[0].docPath)
-        .set(
-          {
-            firstName: afterData.firstName,
-            lastName: afterData.lastName,
-            preferredName: afterData.preferredName,
-            background: afterData.personalBio,
-          },
-          { merge: true }
-        );
+   if(afterData&&afterData.founder[0].docPath)await db.doc(afterData.founder[0].docPath).set({firstName:afterData.firstName,lastName:afterData.lastName,preferredName:afterData.preferredName,background:afterData.personalBio,role:afterData.role},{merge:true})
 
     return true;
   });
