ddc8d0a281858a2b9716af2aa7dd5ffec0e8024d
shams mosowi <shams.mosowi@gmail.com>
2019-10-28 17:38:48 +1100

    update map:profilephoto

--- cloud_functions/functions/src/maps.ts
+++ cloud_functions/functions/src/maps.ts
@@ -8,7 +8,7 @@ export const users = functions.firestore
     const afterData = change.after.data();
     if (afterData) { 
       if( afterData.founder[0].docPath){
-        const updates = {firstName:afterData.firstName,lastName:afterData.lastName,preferredName:afterData.preferredName,background:afterData.personalBio,role:afterData.role}
+        const updates = {firstName:afterData.firstName,lastName:afterData.lastName,preferredName:afterData.preferredName,background:afterData.personalBio,role:afterData.role,profilePhoto:afterData.profilePhoto}
         console.log(`updates FROM users/${context.params.id} TO ${afterData.founder[0].docPath}`, updates)
    await db.doc(afterData.founder[0].docPath).set(updates, { merge: true }); 
       }
