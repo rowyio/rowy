eb63e01af7851bfedfa556de090f528e69f81320
Shams Mosowi <shams@Shamss-MacBook-Pro.local>
2019-12-17 17:03:49 +1100

    image upload

--- www/src/components/RichTextEditor.jsx
+++ www/src/components/RichTextEditor.jsx
@@ -2,13 +2,17 @@ import React, { useContext, useRef, useEffect } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import EditorContext from "contexts/editorContext";
 import { FieldType } from "./Fields";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
+import { ImageDrop } from "quill-image-drop-module";
 import Delta from "quill-delta";
 import "react-quill/dist/quill.snow.css";
 import { bucket } from "../firebase";
 
 import EditorModel from "./EditorModal";
-const useStyles = makeStyles((theme: Theme) =>
+
+Quill.register("modules/imageDrop", ImageDrop);
+
+const useStyles = makeStyles(theme =>
   createStyles({
     test: { position: "absolute", top: 10, left: 10 },
     modal: {
@@ -117,22 +121,20 @@ const useStyles = makeStyles((theme: Theme) =>
     },
   })
 );
-interface Props {}
 
-async function asyncUploader(ref: any, file: File) {
-  const documentRef = bucket.ref("test").child(ref);
+async function asyncUploader(ref, file) {
+  const documentRef = bucket.ref("RTE").child(ref);
   const snapShot = await documentRef.put(file);
   const downloadUrl = await bucket
-    .ref("test")
+    .ref("RTE")
     .child(snapShot.metadata.fullPath)
     .getDownloadURL();
   return downloadUrl;
 }
-const imageHandler = (quillRef: any) => () => {
+const imageHandler = quillRef => () => {
   const now = new Date();
-
   const quill = quillRef.current.getEditor();
-  let fileInput: any = document.body.querySelector("input.ql-image[type=file]");
+  let fileInput = document.body.querySelector("input.ql-image[type=file]");
   if (fileInput == null) {
     fileInput = document.createElement("input");
     fileInput.setAttribute("type", "file");
@@ -142,9 +144,11 @@ const imageHandler = (quillRef: any) => () => {
     );
     fileInput.classList.add("ql-image");
     fileInput.addEventListener("change", async () => {
+      console.log("detected file");
       if (fileInput.files != null && fileInput.files[0] != null) {
         const ref = `quill-images/${now.getTime()}/${fileInput.files[0].name}`;
         const downloadUrl = await asyncUploader(ref, fileInput.files[0]);
+        console.log(downloadUrl);
         let range = quill.getSelection(true);
         quill.updateContents(
           new Delta()
@@ -163,20 +167,19 @@ const imageHandler = (quillRef: any) => () => {
   fileInput.click();
 };
 
-const RichTextEditor = (props: Props) => {
+const RichTextEditor = props => {
   const classes = useStyles();
   const editorContext = useContext(EditorContext);
   const quillRef = useRef(null);
   useEffect(() => {
-    if (quillRef !== null && quillRef.current) {
-      // quillRef.current
-      //   .getEditor()
-      //   .getModule("toolbar")
-      //   .addHandler("image", imageHandler(quillRef));
+    if (quillRef.current) {
+      quillRef.current
+        .getEditor()
+        .getModule("toolbar")
+        .addHandler("image", imageHandler(quillRef));
     }
   }, [quillRef]);
   if (editorContext.fieldType !== FieldType.richText) return <></>;
-  const isOpen = editorContext.editorValue !== null;
   return (
     <EditorModel>
       <ReactQuill
@@ -196,6 +199,10 @@ const RichTextEditor = (props: Props) => {
         className={classes.quillEditor}
         //  preserveWhiteSpace
         modules={{
+          // toolbar: [
+          //   ['image']
+          // ],
+          imageDrop: true,
           toolbar: {
             container: [
               ["bold", "italic", "underline"],
@@ -203,7 +210,7 @@ const RichTextEditor = (props: Props) => {
 
               [{ list: "bullet" }, { list: "ordered" }],
               [{ indent: "-1" }, { indent: "+1" }],
-              ["blockquote", "code-block", "video"],
+              ["blockquote", "code-block", "image", "video"],
               ["link"],
             ],
           },
