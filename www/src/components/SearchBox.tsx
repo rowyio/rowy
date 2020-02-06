b521fd915c053cf86cfa44a0e6a1dbf28b967fa1
Shams mosowi <shams.mosowi@gmail.com>
2020-02-07 07:49:09 +1100

    fix .env

--- www/src/components/SearchBox.tsx
+++ www/src/components/SearchBox.tsx
@@ -14,8 +14,8 @@ import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 
 const searchClient = algoliasearch(
-  "B6E2V80XVH",
-  "b7e09085e151e118f9a548192e2eb0ff"
+  `${process.env.REACT_APP_ALGOLIA_APP_ID}`,
+  `${process.env.REACT_APP_ALGOLIA_SEARCH_KEY}`
 );
 
 const useStyles = makeStyles(() =>
