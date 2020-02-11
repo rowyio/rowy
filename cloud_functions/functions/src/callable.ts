f01b613ee91c4394e0359808db3a8deb70739f6b
Shams mosowi <shams.mosowi@gmail.com>
2020-02-11 15:25:31 +1100

    fix: createInFounders check caller email

--- cloud_functions/functions/src/callable.ts
+++ cloud_functions/functions/src/callable.ts
@@ -15,35 +15,44 @@ export const createInFounders = functions.https.onCall(
   ) => {
     const { row, ref } = data;
     console.log(context.auth);
-    const fieldsToSync = [
-      "firstName",
-      "lastName",
-      "preferredName",
-      "personalBio",
-      "founderType",
-      "cohort",
-      "email",
-      "profilePhoto",
-      "twitter",
-      "employerLogos",
-      "linkedin",
-      "publicProfile",
-      "companies",
-    ];
-    const syncData = fieldsToSync.reduce((acc: any, curr: string) => {
-      if (row[curr]) {
-        acc[curr] = row[curr];
-        return acc;
-      } else return acc;
-    }, {});
-    await db
-      .collection("founders")
-      .doc(ref.id)
-      .set(syncData, { merge: true });
-    return {
-      message: "Founder created!",
-      cellValue: { redo: false, status: "complete" },
-      success: true,
-    };
+
+    if (context.auth && context.auth.token.email.includes("@antler.co")) {
+      const fieldsToSync = [
+        "firstName",
+        "lastName",
+        "preferredName",
+        "personalBio",
+        "founderType",
+        "cohort",
+        "email",
+        "profilePhoto",
+        "twitter",
+        "employerLogos",
+        "linkedin",
+        "publicProfile",
+        "companies",
+      ];
+      const syncData = fieldsToSync.reduce((acc: any, curr: string) => {
+        if (row[curr]) {
+          acc[curr] = row[curr];
+          return acc;
+        } else return acc;
+      }, {});
+      await db
+        .collection("founders")
+        .doc(ref.id)
+        .set(syncData, { merge: true });
+      return {
+        message: "Founder created!",
+        cellValue: { redo: false, status: "complete" },
+        success: true,
+      };
+    } else {
+      return {
+        message: "unauthorized function",
+        cellValue: { redo: false, status: "complete" },
+        success: false,
+      };
+    }
   }
 );
