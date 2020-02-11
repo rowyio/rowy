98b1a4a666150f49643ee6cc41bd717205fa7443
Shams mosowi <shams.mosowi@gmail.com>
2020-02-11 12:57:49 +1100

    callable example:createInFounders

---
+++ cloud_functions/functions/src/callable.ts
@@ -0,0 +1,49 @@
+import * as functions from "firebase-functions";
+import { db } from "./config";
+// example callable function
+export const createInFounders = functions.https.onCall(
+  async (
+    data: {
+      ref: {
+        id: string;
+        path: string;
+        parentId: string;
+      };
+      row: any;
+    },
+    context: functions.https.CallableContext
+  ) => {
+    const { row, ref } = data;
+    console.log(context.auth);
+    const fieldsToSync = [
+      "firstName",
+      "lastName",
+      "preferredName",
+      "personalBio",
+      "founderType",
+      "cohort",
+      "email",
+      "profilePhoto",
+      "twitter",
+      "employerLogos",
+      "linkedin",
+      "publicProfile",
+      "companies",
+    ];
+    const syncData = fieldsToSync.reduce((acc: any, curr: string) => {
+      if (row[curr]) {
+        acc[curr] = row[curr];
+        return acc;
+      } else return acc;
+    }, {});
+    await db
+      .collection("founders")
+      .doc(ref.id)
+      .set(syncData, { merge: true });
+    return {
+      message: "Founder created!",
+      cellValue: { redo: false, status: "complete" },
+      success: true,
+    };
+  }
+);
--- cloud_functions/functions/src/index.ts
+++ cloud_functions/functions/src/index.ts
@@ -1,6 +1,8 @@
-export { exportTable } from "./export";
 import algoliaFnsGenerator from "./algolia";
 import algoliaConfig from "./algolia/algoliaConfig";
+export { exportTable } from "./export";
+import * as callableFns from "./callable";
+export const callable = callableFns;
 export const algolia = algoliaConfig.reduce((acc: any, collection) => {
   return { ...acc, [collection.name]: algoliaFnsGenerator(collection) };
 }, {});
