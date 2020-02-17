e69f7f5308897c96bd09159f2436eebd0ec5ae6f
Shams mosowi <shams.mosowi@gmail.com>
2020-02-17 18:16:02 +1100

    fix merg

--- www/src/components/Table/Grid.tsx
+++ www/src/components/Table/Grid.tsx
@@ -1,77 +1,11 @@
 import EmptyTable from "./EmptyTable";
 import { editable } from "./grid-fns";
-import { DraggableHeader } from "react-data-grid-addons";
-import Loading from "../Loading";
 
+import Loading from "../Loading";
 
 import { FieldType } from "constants/fields";
 import { useSideDrawerContext } from "contexts/sideDrawerContext";
 
-const ReactDataGrid = lazy(() => import("react-data-grid"));
-const { DraggableContainer } = DraggableHeader;
-
-export interface IGridProps {
-  columns: (AdazzleReactDataGrid.Column<any> & {
-    isNew?: boolean;
-    type: FieldType;
-    [key: string]: any;
-  })[];
-  [key: string]: any;
-}
-
-const Grid = ({
-  onHeaderDrop,
-  rowHeight,
-  columns,
-  RowRenderer,
-  handleRowGetter,
-  tableHeight,
-  onGridRowsUpdated,
-  rows,
-  resizeColumn,
-  loadingRows,
-  addRow,
-  setSelectedCell,
-}: IGridProps) => {
-  const { setSelectedCell: contextSetSelectedCell } = useSideDrawerContext();
-
-  return (
-    <Suspense fallback={<Loading message="Loading table" />}>
-      <DraggableContainer onHeaderDrop={onHeaderDrop}>
-        <ReactDataGrid
-          headerRowHeight={47}
-          rowRenderer={RowRenderer}
-          rowHeight={rowHeight}
-          columns={columns}
-          enableCellSelect={true} // makes text based cells editable
-          rowGetter={handleRowGetter}
-          rowsCount={rows.length}
-          onGridRowsUpdated={onGridRowsUpdated}
-          minHeight={tableHeight}
-          onCellSelected={(coordinates: { rowIdx: number; idx: number }) => {
-            const row = rows[coordinates.rowIdx];
-            const column = columns[coordinates.idx];
-            if (editable(column.type)) {
-              //only editable fields are stored selectedCell, temporary fix for custom fields
-              setSelectedCell({ row, column });
-            }
-            if (contextSetSelectedCell) contextSetSelectedCell({ row });
-          }}
-          onColumnResize={(idx: number, width: number) =>
-            //tableActions.column.resize(idx, width)
-            resizeColumn(idx, width)
-          }
-          emptyRowsView={() => (
-            <EmptyTable
-              //isLoading={tableState.loadingRows}
-              isLoading={loadingRows}
-              tableHeight={tableHeight}
-              addRow={addRow}
-            />
-          )}
-        />
-      </DraggableContainer>
-    </Suspense>
 import "react-data-grid/dist/react-data-grid.css";
 
 import React, { useState, useCallback, useRef } from "react";
@@ -86,23 +20,69 @@ import DataGrid, {
   CellNavigationMode,
 } from "react-data-grid";
 
-function ImageFormatter({
-  value,
-}: {
-  /** image url, used as background-image */
-  value: string;
-}) {
-  return (
-    <div className="rdg-image-cell-wrapper">
-      <div
-        className="rdg-image-cell"
-        style={{ backgroundImage: `url(${value})` }}
-      />
-    </div>
-  );
+export interface IGridProps {
+  columns: (Column<any> & {
+    isNew?: boolean;
+    type: FieldType;
+    [key: string]: any;
+  })[];
+  [key: string]: any;
 }
 
-const { DraggableContainer } = DraggableHeader;
+// const Grid = ({
+//   onHeaderDrop,
+//   rowHeight,
+//   columns,
+//   RowRenderer,
+//   handleRowGetter,
+//   tableHeight,
+//   onGridRowsUpdated,
+//   rows,
+//   resizeColumn,
+//   loadingRows,
+//   addRow,
+//   setSelectedCell,
+// }: IGridProps) => {
+//   const { setSelectedCell: contextSetSelectedCell } = useSideDrawerContext();
+
+//   return (
+//     <Suspense fallback={<Loading message="Loading table" />}>
+//       <DraggableContainer onHeaderDrop={onHeaderDrop}>
+//         <ReactDataGrid
+//           headerRowHeight={47}
+//           rowRenderer={RowRenderer}
+//           rowHeight={rowHeight}
+//           columns={columns}
+//           enableCellSelect={true} // makes text based cells editable
+//           rowGetter={handleRowGetter}
+//           rowsCount={rows.length}
+//           onGridRowsUpdated={onGridRowsUpdated}
+//           minHeight={tableHeight}
+//           onCellSelected={(coordinates: { rowIdx: number; idx: number }) => {
+//             const row = rows[coordinates.rowIdx];
+//             const column = columns[coordinates.idx];
+//             if (editable(column.type)) {
+//               //only editable fields are stored selectedCell, temporary fix for custom fields
+//               setSelectedCell({ row, column });
+//             }
+//             if (contextSetSelectedCell) contextSetSelectedCell({ row });
+//           }}
+//           onColumnResize={(idx: number, width: number) =>
+//             //tableActions.column.resize(idx, width)
+//             resizeColumn(idx, width)
+//           }
+//           emptyRowsView={() => (
+//             <EmptyTable
+//               //isLoading={tableState.loadingRows}
+//               isLoading={loadingRows}
+//               tableHeight={tableHeight}
+//               addRow={addRow}
+//             />
+//           )}
+//         />
+//       </DraggableContainer>
+//     </Suspense>
+
 const Grid = (props: any) => {
   const {
     onHeaderDrop,
