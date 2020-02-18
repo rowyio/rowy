ce8a10d99cf50ea769a223d86d2f99c3d3a0218f
Shams mosowi <shams.mosowi@gmail.com>
2020-02-18 19:41:38 +1100

    fixed merge

--- www/src/components/Navigation.tsx
+++ www/src/components/Navigation.tsx
@@ -19,7 +19,6 @@ import SideDrawer, { DRAWER_COLLAPSED_WIDTH } from "./SideDrawer";
 
 import useRouter from "../hooks/useRouter";
 
-import { SideDrawerProvider } from "contexts/sideDrawerContext";
 import { useFiretableContext } from "../contexts/firetableContext";
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -84,136 +83,109 @@ const Navigation = (props: any) => {
     }
   }, [table]);
   return (
-    <SideDrawerProvider>
-        {section && sections && (
-          <Tabs
-            value={table}
-            indicatorColor="primary"
-            textColor="primary"
-            onChange={(e, v) => {
-              setTable(v);
-            }}
-            aria-label="disabled tabs example"
+    <>
+      {section && sections && (
+        <Tabs
+          value={table}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(e, v) => {
+            setTable(v);
+          }}
+          aria-label="disabled tabs example"
+        >
+          {sections[section].map(table => (
+            <Tab key={table.name} label={table.name} value={table} />
+          ))}
+        </Tabs>
+      )}
+      {props.children}
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="go home"
+            component={Link}
+            to="/"
+            className={classes.homeButton}
           >
-            {sections[section].map(table => (
-              <Tab key={table.name} label={table.name} value={table} />
-            ))}
-          </Tabs>
-        )}
-        {props.children}
-        <AppBar position="fixed" color="primary" className={classes.appBar}>
-          <Toolbar className={classes.toolbar}>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="go home"
-              component={Link}
-              to="/"
-              className={classes.homeButton}
+            <HomeIcon />
+          </IconButton>
+
+          {!sections ? (
+            <>
+              <Skeleton
+                variant="rect"
+                width={120}
+                height={40}
+                className={classes.skeleton}
+              />
+              <Skeleton
+                variant="rect"
+                width={120}
+                height={40}
+                className={classes.skeleton}
+              />
+              <Skeleton
+                variant="rect"
+                width={120}
+                height={40}
+                className={classes.skeleton}
+              />
+              <Skeleton
+                variant="rect"
+                width={120}
+                height={40}
+                className={classes.skeleton}
+              />
+            </>
+          ) : (
+            <Grid
+              container
+              className={classes.routes}
+              wrap="nowrap"
+              alignItems="center"
+              spacing={2}
             >
-              <HomeIcon />
-            </IconButton>
-
-            {!sections ? (
-              <>
-                <Skeleton
-                  variant="rect"
-                  width={120}
-                  height={40}
-                  className={classes.skeleton}
-                />
-                <Skeleton
-                  variant="rect"
-                  width={120}
-                  height={40}
-                  className={classes.skeleton}
-                />
-                <Skeleton
-                  variant="rect"
-                  width={120}
-                  height={40}
-                  className={classes.skeleton}
-                />
-                <Skeleton
-                  variant="rect"
-                  width={120}
-                  height={40}
-                  className={classes.skeleton}
-                />
-              </>
-            ) : (
-              <Grid
-                container
-                className={classes.routes}
-                wrap="nowrap"
-                alignItems="center"
-                spacing={2}
-              >
-
-                {settings.tables.map(
-                  (table: { name: string; collection: string }) => (
-                    <Grid item key={table.collection}>
-                      <Button
-                        key={table.collection}
-                        component={Link}
-                        to={table.collection}
-                        disabled={
-                          table.collection ===
-                          router.location.pathname.replace("/table/", "")
-                        }
-                        color="inherit"
-                        className={classes.routeButton}
-                        classes={{
-                          root: classes.routeButton,
-                          disabled: classes.currentRouteButton,
-                        }}
-                      >
-                        {table.name}
-                      </Button>
-                    </Grid>
-                  )
-                )}
-                {Object.keys(sections).map((sectionName: string) => (
-                  <Grid item key={sectionName}>
-                    <Button
-                      key={sectionName}
-                      // component={Link}
-                      // to={table.collection}
-                      disabled={sectionName === section}
-                      onClick={() => {
-                        setSection(sectionName);
-                      }}
-                      color="inherit"
-                      className={classes.routeButton}
-                      classes={{
-                        root: classes.routeButton,
-                        disabled: classes.currentRouteButton,
-                      }}
-                    >
-                      {sectionName}
-                    </Button>
-                  </Grid>
-                ))}
-                <Grid item>
-                  <div className={classes.routeSpacer} />
+              {Object.keys(sections).map((sectionName: string) => (
+                <Grid item key={sectionName}>
+                  <Button
+                    key={sectionName}
+                    // component={Link}
+                    // to={table.collection}
+                    disabled={sectionName === section}
+                    onClick={() => {
+                      setSection(sectionName);
+                    }}
+                    color="inherit"
+                    className={classes.routeButton}
+                    classes={{
+                      root: classes.routeButton,
+                      disabled: classes.currentRouteButton,
+                    }}
+                  >
+                    {sectionName}
+                  </Button>
                 </Grid>
+              ))}
+              <Grid item>
+                <div className={classes.routeSpacer} />
               </Grid>
-            )}
-            {/* <Button
+            </Grid>
+          )}
+          {/* <Button
               onClick={() => {
                 auth.signOut();
               }}
             >
               Sign out
             </Button> */}
+        </Toolbar>
+      </AppBar>
 
-          </Toolbar>
-        </AppBar>
-
-        <SideDrawer />
-      </>
-
-    </SideDrawerProvider>
+      <SideDrawer />
+    </>
   );
 };
 export default Navigation;
