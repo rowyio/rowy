5fd3d43cf088796d5591331a22947be8c6a3bda3
Sidney Alcantara <sidney@sidney.me>
2020-02-27 10:45:31 +1100

    fix env variables

--- www/src/components/ConnectTableSelect/PopupContents.tsx
+++ www/src/components/ConnectTableSelect/PopupContents.tsx
@@ -26,7 +26,7 @@ import { Response } from "algoliasearch";
 
 const searchClient = algoliasearch(
   process.env.REACT_APP_ALGOLIA_APP_ID ?? "",
-  process.env.REACT_APP_ALGOLIA_SEARCH_KEY ?? ""
+  process.env.REACT_APP_ALGOLIA_SEARCH_API_KEY ?? ""
 );
 console.log("SEARCH CLIENT", searchClient);
 
