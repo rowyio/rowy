8ea2df38e3bb6e6d5453556d57641223ec403d41
Shams mosowi <shams.mosowi@gmail.com>
2020-04-02 15:56:52 +0800

    remove antler callables

--- cloud_functions/functions/src/callable.ts
+++ cloud_functions/functions/src/callable.ts
@@ -1,81 +1,10 @@
 import * as functions from "firebase-functions";
 import * as _ from "lodash";
-import { db } from "./config";
 import * as admin from "firebase-admin";
 import { sendEmail } from "./utils/email";
 import { hasAnyRole } from "./utils/auth";
 
 const serverTimestamp = admin.firestore.FieldValue.serverTimestamp;
-
-export const verifyFounder = functions.https.onCall(
-  async (
-    data: {
-      ref: {
-        id: string;
-        path: string;
-        parentId: string;
-      };
-      row: any;
-      action: "run" | "redo" | "undo";
-    },
-    context: functions.https.CallableContext
-  ) => {
-    const { row, ref, action } = data;
-    const fieldsToSync = [
-      "firstName",
-      "lastName",
-      "preferredName",
-      "personalBio",
-      "founderType",
-      "cohort",
-      "email",
-      "profilePhoto",
-      "twitter",
-      "employerLogos",
-      "linkedin",
-      "publicProfile",
-      "companies",
-    ];
-
-    const authorized = hasAnyRole(["ADMIN", "PROGRAM"], context);
-
-    if (!context.auth || !authorized) {
-      console.warn(`unauthorized user${context}`);
-      return {
-        success: false,
-        message: "you don't have permissions to send this email",
-      };
-    }
-    switch (action) {
-      case "undo":
-      case "redo":
-      case "run":
-      default:
-        const syncData = fieldsToSync.reduce((acc: any, curr: string) => {
-          if (row[curr]) {
-            acc[curr] = row[curr];
-            return acc;
-          } else return acc;
-        }, {});
-        await db
-          .collection("founders")
-          .doc(ref.id)
-          .set(syncData, { merge: true });
-        await sendEmail("p8K9z0CBhGlb3Vgzl02o", row);
-        return {
-          message: "Founder created!",
-          cellValue: {
-            redo: false,
-            status: "Verified",
-            undo: true,
-            meta: { ranBy: context.auth.token.email },
-          },
-          completedAt: serverTimestamp(),
-          success: true,
-        };
-    }
-  }
-);
 
 const sendEmailTemplateCallable = async (
   data: {
@@ -116,46 +45,3 @@ const sendEmailTemplateCallable = async (
 };
 
 export const SendEmail = functions.https.onCall(sendEmailTemplateCallable);
-
-const dissolveTeam = async (
-  data: {
-    ref: {
-      id: string;
-      path: string;
-      parentId: string;
-    };
-    row: any;
-    column: any;
-    action: "run" | "redo" | "undo";
-  },
-  context: functions.https.CallableContext
-) => {
-  const authorized = hasAnyRole(["ADMIN", "PROGRAM"], context);
-
-  if (!context.auth || !authorized) {
-    console.warn(`unauthorized user${{ context }}`);
-    return {
-      success: false,
-      message: "you don't have permissions to dissolve this team",
-    };
-  }
-
-  await db
-    .collection("myTeam")
-    .doc(data.ref.id)
-    .update({ isDissolved: true });
-
-  return {
-    message: "team dissolved",
-    cellValue: {
-      redo: false,
-      status: `Dissolved`,
-      completedAt: serverTimestamp(),
-      meta: { ranBy: context.auth.token.email },
-      undo: false,
-    },
-    success: true,
-  };
-};
-
-export const DissolveTeam = functions.https.onCall(dissolveTeam);
