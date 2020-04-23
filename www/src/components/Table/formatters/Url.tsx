5544a76020d72199ab0e0978201fde1017691518
Sidney Alcantara <sidney@sidney.me>
2020-04-23 11:36:53 +1000

    fix Url field style

--- www/src/components/Table/formatters/Url.tsx
+++ www/src/components/Table/formatters/Url.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { CustomCellProps } from "./withCustomCell";
 
+import { Link } from "@material-ui/core";
+
 export default function Url({ value }: CustomCellProps) {
   return (
-    <a href={value} target="_blank" rel="noopener noreferrer">
+    <Link
+      href={value}
+      target="_blank"
+      rel="noopener noreferrer"
+      underline="always"
+      style={{ fontWeight: "bold" }}
+    >
       {value}
-    </a>
+    </Link>
   );
 }
