91349c092093b4c6337889829571003f4a634b09
Shams mosowi <shams.mosowi@gmail.com>
2020-08-08 23:49:55 +1000

    firetable cli deployment tool

---
+++ cli/lib/files.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  getCurrentDirectoryBase: () => {
+    return path.basename(process.cwd());
+  },
+
+  directoryExists: filePath => {
+    return fs.existsSync(filePath);
+  },
+};
---
+++ cli/logo.js
@@ -0,0 +1,37 @@
+module.exports.logo = `tttttttttttttttttttttttttttttttttttttttt
+tttttttttttttttttttttttttttttttttttttttt
+tttttttttttttttttttttttt            tttt
+tttttttttttttttttttttttt     A      tttt
+tttttttttttttttttttttttt   AA AA    tttt
+tttttttttttttttttttttttt  AA   AA   tttt
+tttttttttttttttttttttttt AAA   AAA  tttt
+tttttttttttttt                      tttt
+tttttttttttttt  tttttttt  ttttttt   tttt
+tttttttttttttt  tttttttt  ttttttt   tttt
+tttttttttttttt  tttttttt  ttttttt   tttt
+tttt                                tttt
+tttt   ttttttt  tttttttt  ttttttt   tttt
+tttt   ttttttt  tttttttt  ttttttt   tttt
+tttt   ttttttt  tttttttt  ttttttt   tttt
+tttt                                tttt
+tttttttttttttttttttttttttttttttttttttttt
+tttttttttttttttttttttttttttttttttttttttt`;
+
+module.exports.logoO = `++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++            ++++
+++++++++++++++++++++++++     A      ++++
+++++++++++++++++++++++++   AA AA    ++++
+++++++++++++++++++++++++  AA   AA   ++++
+++++++++++++++++++++++++ AAA   AAA  ++++
+++++++++++++++                      ++++
+++++++++++++++  ++++++++  +++++++   ++++
+++++++++++++++  ++++++++  +++++++   ++++
+++++++++++++++  ++++++++  +++++++   ++++
+++++                                ++++
+++++   +++++++  ++++++++  +++++++   ++++
+++++   +++++++  ++++++++  +++++++   ++++
+++++   +++++++  ++++++++  +++++++   ++++
+++++                                ++++
+++++++++++++++++++++++++++++++++++++++++
+++++++++++++++++++++++++++++++++++++++++`;
