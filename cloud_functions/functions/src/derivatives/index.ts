0634db0cd0aef86ecb0d20e3ae246c7b0a059c4c
Shams mosowi <shams.mosowi@gmail.com>
2020-08-25 09:59:25 +1000

    fix derivatives with - in collection names

--- cloud_functions/functions/src/derivatives/index.ts
+++ cloud_functions/functions/src/derivatives/index.ts
@@ -64,7 +64,7 @@ export const derivativeOnUpdate = async (
 };
 
 export const FT_derivatives = {
-  [collectionPath]: {
+  [collectionPath.replace("-", "_")]: {
     onUpdate: functions.firestore
       .document(`${collectionPath}/{docId}`)
       .onUpdate(derivativeOnUpdate),
