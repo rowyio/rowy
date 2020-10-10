72d1663f354a7e419632bd3b1d17a860ef83a550
Shams mosowi <shams.mosowi@gmail.com>
2020-10-10 14:58:01 +1100

    cleanup row getter

--- www/src/components/Table/index.tsx
+++ www/src/components/Table/index.tsx
@@ -123,17 +123,15 @@ export default function Table() {
 
   const rowHeight = tableState.config.rowHeight;
   const rows = tableState.rows;
-  //const rowGetter = (rowIdx: number) => rows[rowIdx];
-  const rowGetter = (rowIdx: number) => ({
-    ...rows[rowIdx],
-    ...columns.reduce(
+
+  const rowGetter = (rowIdx: number) =>
+    columns.reduce(
       (acc, currColumn) => ({
         ...acc,
         [currColumn.key]: _get(rows[rowIdx], currColumn.key),
       }),
       {}
-    ),
-  });
+    );
 
   let tableWidth: any = `calc(100% - ${
     DRAWER_COLLAPSED_WIDTH
