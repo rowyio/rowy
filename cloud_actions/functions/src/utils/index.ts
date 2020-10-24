f71932818c484e2f750a1255e5f253dbe04965dc
Shams mosowi <shams.mosowi@gmail.com>
2020-10-24 11:55:25 +1100

    fix configs

--- cloud_actions/functions/src/utils/index.ts
+++ cloud_actions/functions/src/utils/index.ts
@@ -3,36 +3,40 @@ export const serverTimestamp = admin.firestore.FieldValue.serverTimestamp;
 import { sendEmail } from "./email";
 import { hasAnyRole } from "./auth";
 
-var characters =
+const characters =
   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-export function generateId(length:number):string {
-  var result = "";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
+export function generateId(length: number): string {
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
 
-
-
-
- const hasRequiredFields = (requiredFields: string[], data: any) =>
+const hasRequiredFields = (requiredFields: string[], data: any) =>
   requiredFields.reduce((acc: boolean, currField: string) => {
     if (data[currField] === undefined || data[currField] === null) return false;
     else return acc;
   }, true);
- async function asyncForEach(array: any[], callback: Function) {
+async function asyncForEach(array: any[], callback: Function) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
 }
- const identifyTriggerType = (beforeData:any, afterData:any) =>
+const identifyTriggerType = (beforeData: any, afterData: any) =>
   Boolean(beforeData) && Boolean(afterData)
     ? "update"
     : Boolean(afterData)
     ? "create"
     : "delete";
 
-
-    export default { hasRequiredFields,generateId, sendEmail, serverTimestamp, hasAnyRole,asyncForEach,identifyTriggerType };
+export default {
+  hasRequiredFields,
+  generateId,
+  sendEmail,
+  serverTimestamp,
+  hasAnyRole,
+  asyncForEach,
+  identifyTriggerType,
+};
