2f3ccb871019a8eb4e0d87a0618d0e1793e40855
Sidney Alcantara <sidney@sidney.me>
2020-11-04 19:58:19 +1100

    fix HomePage styled card font

--- www/src/components/RichText.tsx
+++ www/src/components/RichText.tsx
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme) =>
   createStyles({
     "@global": {
       body: {
-        fontFamily: theme.typography.fontFamily,
+        fontFamily: theme.typography.fontFamily + " !important",
       },
     },
 
