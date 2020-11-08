0dfdf9028cdc6ef949e47d3f08db448f43af50b3
Shams mosowi <shams.mosowi@gmail.com>
2020-11-09 07:54:46 +0800

    add keys for operator options

--- www/src/components/Table/Filters/index.tsx
+++ www/src/components/Table/Filters/index.tsx
@@ -297,6 +297,10 @@ const Filters = () => {
             freeText={true}
           />
         );
+
+      case FieldType.date:
+      case FieldType.dateTime:
+        return <>//TODO:Date Time picker</>;
       default:
         return <>Not available</>;
         // return <TextField variant="filled" fullWidth disabled />;
@@ -442,7 +446,7 @@ const Filters = () => {
                   Select Condition
                 </MenuItem>
                 {operators.map((operator) => (
-                  <MenuItem value={operator.value}>{operator.label}</MenuItem>
+                  <MenuItem key={operator.value} value={operator.value}>{operator.label}</MenuItem>
                 ))}
               </TextField>
             </Grid>
