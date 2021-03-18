17f242bfa95323ba7072b499824cd7040c236655
Bobby <bofei@mail.com>
2021-03-18 21:17:44 +1100

    update monaco editor to 4.1.0

--- www/src/components/Table/editors/CodeEditor.tsx
+++ www/src/components/Table/editors/CodeEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useMemo, useState } from "react";
 import { useTheme, createStyles, makeStyles } from "@material-ui/core/styles";
-import Editor, { monaco } from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 import { useFiretableContext } from "contexts/FiretableContext";
 import { FieldType } from "constants/fields";
 import { setTimeout } from "timers";
@@ -24,6 +24,7 @@ const useStyles = makeStyles((theme) =>
 export default function CodeEditor(props: any) {
   const { handleChange, extraLibs, script, height = 400 } = props;
   const theme = useTheme();
+  const monacoInstance = useMonaco();
 
   const [initialEditorValue] = useState(script ?? "");
   const { tableState } = useFiretableContext();
@@ -35,15 +36,22 @@ export default function CodeEditor(props: any) {
     editorRef.current = editor;
   }
 
-  function listenEditorChanges() {
-    setTimeout(() => {
-      editorRef.current?.onDidChangeModelContent((ev) => {
-        handleChange(editorRef.current.getValue());
-      });
-    }, 2000);
-  }
+  const themeTransformer = (theme: string) => {
+    switch (theme) {
+      case "dark":
+        return "vs-dark";
+      default:
+        return theme;
+    }
+  };
 
   useMemo(async () => {
+    if (!monacoInstance) {
+      // useMonaco returns a monaco instance but initialisation is done asynchronously
+      // dont execute the logic until the instance is initialised
+      return;
+    }
+
     const firestoreDefsFile = await fetch(
       `${process.env.PUBLIC_URL}/firestore.d.ts`
     );
@@ -53,107 +61,107 @@ export default function CodeEditor(props: any) {
     const firestoreDefs = await firestoreDefsFile.text();
     // const firebaseAuthDefs = await firebaseAuthDefsFile.text();
     // console.timeLog(firebaseAuthDefs);
-    monaco
-      .init()
-      .then((monacoInstance) => {
-        monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
-          firestoreDefs
-        );
-        // monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
-        //   firebaseAuthDefs
-        // );
-        monacoInstance.languages.typescript.javascriptDefaults.setDiagnosticsOptions(
-          {
-            noSemanticValidation: true,
-            noSyntaxValidation: false,
-          }
-        );
-        // compiler options
-        monacoInstance.languages.typescript.javascriptDefaults.setCompilerOptions(
-          {
-            target: monacoInstance.languages.typescript.ScriptTarget.ES5,
-            allowNonTsExtensions: true,
-          }
-        );
-        if (extraLibs) {
-          monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
-            extraLibs.join("\n"),
-            "ts:filename/extraLibs.d.ts"
-          );
+    // monaco
+    //   .init()
+    //   .then((monacoInstance) => {
+    try {
+      monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
+        firestoreDefs
+      );
+      // monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
+      //   firebaseAuthDefs
+      // );
+      monacoInstance.languages.typescript.javascriptDefaults.setDiagnosticsOptions(
+        {
+          noSemanticValidation: true,
+          noSyntaxValidation: false,
         }
+      );
+      // compiler options
+      monacoInstance.languages.typescript.javascriptDefaults.setCompilerOptions(
+        {
+          target: monacoInstance.languages.typescript.ScriptTarget.ES5,
+          allowNonTsExtensions: true,
+        }
+      );
+      if (extraLibs) {
         monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
-          [
-            "    /**",
-            "     * utility functions",
-            "     */",
-            "declare namespace utilFns {",
-            "    /**",
-            "     * Sends out an email through sendGrid",
-            "     */",
-            `function sendEmail(msg:{from: string,
+          extraLibs.join("\n"),
+          "ts:filename/extraLibs.d.ts"
+        );
+      }
+      monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
+        [
+          "    /**",
+          "     * utility functions",
+          "     */",
+          "declare namespace utilFns {",
+          "    /**",
+          "     * Sends out an email through sendGrid",
+          "     */",
+          `function sendEmail(msg:{from: string,
               templateId:string,
               personalizations:{to:string,dynamic_template_data:any}[]}):void {
 
               }`,
-            "}",
-          ].join("\n"),
-          "ts:filename/utils.d.ts"
-        );
-
-        monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
-          [
-            "  const db:FirebaseFirestore.Firestore;",
-            //     "  const auth:admin.auth;",
-            "declare class row {",
-            "    /**",
-            "     * Returns the row fields",
-            "     */",
-            ...Object.keys(tableState?.columns!).map((columnKey: string) => {
-              const column = tableState?.columns[columnKey];
-              switch (column.type) {
-                case FieldType.shortText:
-                case FieldType.longText:
-                case FieldType.email:
-                case FieldType.phone:
-                case FieldType.code:
-                  return `static ${columnKey}:string`;
-                case FieldType.singleSelect:
-                  const typeString = [
-                    ...column.config.options.map((opt) => `"${opt}"`),
-                    //     "string",
-                  ].join(" | ");
-                  return `static ${columnKey}:${typeString}`;
-                case FieldType.multiSelect:
-                  return `static ${columnKey}:string[]`;
-                case FieldType.checkbox:
-                  return `static ${columnKey}:boolean`;
-                default:
-                  return `static ${columnKey}:any`;
-              }
-            }),
-            "}",
-          ].join("\n"),
-          "ts:filename/rowFields.d.ts"
-        );
-        //  monacoInstance.editor.create(wrapper, properties);
-      })
-      .catch((error) =>
-        console.error(
-          "An error occurred during initialization of Monaco: ",
-          error
-        )
+          "}",
+        ].join("\n"),
+        "ts:filename/utils.d.ts"
       );
-    listenEditorChanges();
-  }, [tableState?.columns]);
+
+      monacoInstance.languages.typescript.javascriptDefaults.addExtraLib(
+        [
+          "  const db:FirebaseFirestore.Firestore;",
+          //     "  const auth:admin.auth;",
+          "declare class row {",
+          "    /**",
+          "     * Returns the row fields",
+          "     */",
+          ...Object.keys(tableState?.columns!).map((columnKey: string) => {
+            const column = tableState?.columns[columnKey];
+            switch (column.type) {
+              case FieldType.shortText:
+              case FieldType.longText:
+              case FieldType.email:
+              case FieldType.phone:
+              case FieldType.code:
+                return `static ${columnKey}:string`;
+              case FieldType.singleSelect:
+                const typeString = [
+                  ...column.config.options.map((opt) => `"${opt}"`),
+                  //     "string",
+                ].join(" | ");
+                return `static ${columnKey}:${typeString}`;
+              case FieldType.multiSelect:
+                return `static ${columnKey}:string[]`;
+              case FieldType.checkbox:
+                return `static ${columnKey}:boolean`;
+              default:
+                return `static ${columnKey}:any`;
+            }
+          }),
+          "}",
+        ].join("\n"),
+        "ts:filename/rowFields.d.ts"
+      );
+    } catch (error) {
+      console.error(
+        "An error occurred during initialization of Monaco: ",
+        error
+      );
+    }
+  }, [tableState?.columns, monacoInstance]);
+
   return (
     <>
       <div className={classes.editorWrapper}>
         <Editor
-          theme={theme.palette.type}
+          theme={themeTransformer(theme.palette.type)}
           height={height}
-          editorDidMount={handleEditorDidMount}
+          onMount={handleEditorDidMount}
           language="javascript"
           value={initialEditorValue}
+          onChange={handleChange}
         />
       </div>
     </>
