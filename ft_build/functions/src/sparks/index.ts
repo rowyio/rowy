0667ea357b755536bc67857117385eff94242bc0
Scott Waddell <scott@sortd.io>
2021-04-20 16:57:14 +1000

    fix lint error

--- ft_build/functions/src/sparks/index.ts
+++ ft_build/functions/src/sparks/index.ts
@@ -1,7 +1,6 @@
 import * as functions from "firebase-functions";
-import { hasRequiredFields, getTriggerType } from "../utils";
+import utilFns, { hasRequiredFields, getTriggerType } from "../utils";
 import { db, auth } from "../firebaseConfig";
-import utilFns from "../utils";
 
 const spark = (sparkConfig) => async (
   change: functions.Change<functions.firestore.DocumentSnapshot>,
@@ -28,7 +27,7 @@ const spark = (sparkConfig) => async (
       change,
       triggerType,
       sparkConfig,
-      utilFns
+      utilFns,
     };
     if (!triggers.includes(triggerType)) return false; //check if trigger type is included in the spark
     if (
@@ -46,7 +45,7 @@ const spark = (sparkConfig) => async (
           : shouldRun)
       : false; //
 
-    console.log(label,"type is ", type, "dontRun value is", dontRun);
+    console.log(label, "type is ", type, "dontRun value is", dontRun);
 
     if (dontRun) return false;
     const sparkData = await Object.keys(sparkBody).reduce(
@@ -64,11 +63,10 @@ const spark = (sparkConfig) => async (
     await sparkFn(sparkData, sparkContext);
     return true;
   } catch (err) {
-    const {
-      label,
-      type,
-    } = sparkConfig;
-    console.log(`error in ${label} spark of type ${type}, on ${context.eventType} in Doc ${context.resource.name}`)
+    const { label, type } = sparkConfig;
+    console.log(
+      `error in ${label} spark of type ${type}, on ${context.eventType} in Doc ${context.resource.name}`
+    );
     console.error(err);
     return Promise.reject(err);
   }
