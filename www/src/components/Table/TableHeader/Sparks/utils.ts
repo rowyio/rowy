9de812b05d715b42bf1329a858cad19ffbb0e5a3
Bobby <bofei@mail.com>
2021-07-02 22:54:49 +1000

    spark default body on create

--- www/src/components/Table/TableHeader/Sparks/utils.ts
+++ www/src/components/Table/TableHeader/Sparks/utils.ts
@@ -30,8 +30,6 @@ interface ISpark {
   requiredFields: string[];
   sparkBody: string;
   shouldRun: boolean | string;
-
-  // TODO break spark body into editable fields
 }
 
 const triggerTypes: ISparkTrigger[] = ["create", "update", "delete"];
@@ -49,15 +47,109 @@ const sparkTypes: ISparkType[] = [
   "twilioMessage",
 ];
 
+const sparkBodyTemplate = {
+  task: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    console.log("Spark is invoked.")
+}`,
+  docSync: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+        fieldsToSync: [],    // a list of string of column names
+        row: row,    // object of data to sync, usually the row itself
+        targetPath: "",  // fill in the path here
+    }
+    return body
+}`,
+  historySnapshot: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+        trackedFields: [],    // a list of string of column names
+    }
+    return body
+}`,
+  algoliaIndex: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+        fieldsToSync: [],    // a list of string of column names
+        row: row,    // object of data to sync, usually the row itself
+        index: "",    // algolia index to sync to
+        objectID: ref.id,    // algolia object ID, ref.id is one possible choice
+    }
+    return body
+}`,
+  meiliIndex: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+        fieldsToSync: [],    // a list of string of column names
+        row: row,    // object of data to sync, usually the row itself
+        index: "",    // algolia index to sync to
+        objectID: ref.id,    // algolia object ID, ref.id is one possible choice
+    }
+    return body
+}`,
+  bigqueryIndex: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+        fieldsToSync: [],    // a list of string of column names
+        row: row,    // object of data to sync, usually the row itself
+        index: "",    // algolia index to sync to
+        objectID: ref.id,    // algolia object ID, ref.id is one possible choice
+    }
+    return body
+}`,
+  slackMessage: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+        channels: [],    // a list of slack channel IDs in string
+        blocks: [],    // the blocks parameter to pass in to slack api
+        text: "",    // the text parameter to pass in to slack api
+        attachments: [],    // the attachments parameter to pass in to slack api
+    }
+    return body
+}`,
+  sendgridEmail: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+        from: "Name<example@domain.com>",   // send from field
+        personalizations: [
+            {
+                to: [{ name: "", email: "" }],  // recipient
+                dynamic_template_data: {
+                },  // template parameters
+            },
+        ],
+        template_id: "",    // sendgrid template ID
+        categories: [], // helper info to categorise sendgrid emails
+    }
+    return body
+}`,
+  apiCall: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+    }
+    return body
+}`,
+  twilioMessage: `async({row, db, change, ref}) => {
+    // feel free to add your own code logic here
+    const body = {
+    }
+    return body
+}`,
+};
+
 function emptySparkObject(type: ISparkType, user: ISparkEditor): ISpark {
   return {
     name: "Unnamed",
     active: false,
     triggers: [],
     type,
-    sparkBody: "{}",
+    sparkBody: sparkBodyTemplate[type] ?? sparkBodyTemplate["task"],
     requiredFields: [],
-    shouldRun: "return true;",
+    shouldRun: `async({row, change}) => {
+    // feel free to add your own code logic here
+    return true;
+}`,
     lastEditor: user,
   };
 }
@@ -67,30 +159,5 @@ function serialiseSpark(sparks: ISpark[]): string {
   return "[]";
 }
 
-/* Convert ft-build spark config string into spark objects */
-function parseSparkConfig(sparkConfig): ISpark[] {
-  try {
-    // remove leading "sparks.config(" and trailing ")"
-    const sanitisedSparks = sparkConfig
-      .replace(/^(\s*)sparks.config\(/, "")
-      .replace(/\);?\s*$/, "");
-    // const sparks = JSON.parse(sanitisedSparks);
-    // const sparks = eval(sanitisedSparks);
-    console.log(sanitisedSparks);
-    console.log(eval(sanitisedSparks));
-    // return sparks as ISpark[];
-    return [];
-  } catch (e) {
-    console.log("error parsing sparks", e);
-    return [];
-  }
-}
-
-export {
-  parseSparkConfig,
-  serialiseSpark,
-  sparkTypes,
-  triggerTypes,
-  emptySparkObject,
-};
+export { serialiseSpark, sparkTypes, triggerTypes, emptySparkObject };
 export type { ISpark, ISparkType, ISparkEditor };
