a092d5c71431703f89cb32b2f3da57249ec5b4af
Sidney Alcantara <sidney@sidney.me>
2021-08-06 21:53:49 +1000

    clean slate with CRA + ts

--- www/src/App.test.tsx
+++ www/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
