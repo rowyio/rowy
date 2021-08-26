a0b4cc72c954c98abb75a16ad24021d6e54d3313
Sidney Alcantara <sidney@sidney.me>
2021-08-26 15:34:27 +1000

    pushback arch rewrite to v3

--- www/src/App.test.tsx
+++ www/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
