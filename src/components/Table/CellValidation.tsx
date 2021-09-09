e7bfd4e943e37c6e60fb319fca647a1381fecb0b
Sidney Alcantara <sidney@sidney.me>
2021-09-09 18:54:23 +1000

    CellValidation: add box-shadow to improve dot appearance

--- src/components/Table/CellValidation.tsx
+++ src/components/Table/CellValidation.tsx
@@ -29,6 +29,11 @@ const Dot = styled("div")(({ theme }) => ({
 
   borderRadius: "50%",
   backgroundColor: theme.palette.error.main,
+
+  boxShadow: `0 0 0 4px var(--background-color)`,
+  ".rdg-row:hover &": {
+    boxShadow: `0 0 0 4px var(--row-hover-background-color)`,
+  },
 }));
 
 export interface ICellValidationProps
