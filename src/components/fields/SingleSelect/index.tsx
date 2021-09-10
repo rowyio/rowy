227e261a23ef0a96992bd5b74a6bcb885a03e123
Sidney Alcantara <sidney@sidney.me>
2021-09-10 13:20:46 +1000

    fix single select field group

--- src/components/fields/SingleSelect/index.tsx
+++ src/components/fields/SingleSelect/index.tsx
@@ -24,7 +24,7 @@ const Settings = lazy(
 export const config: IFieldConfig = {
   type: FieldType.singleSelect,
   name: "Single Select",
-  group: "File",
+  group: "Select",
   dataType: "string | null",
   initialValue: null,
   initializable: true,
