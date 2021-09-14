1b6274c22ea1ce562a5498ba261c30b6568ceb27
shamsmosowi <shams.mosowi@gmail.com>
2021-09-14 10:44:56 +1000

    defined routes

--- src/pages/RowyRunTest.tsx
+++ src/pages/RowyRunTest.tsx
@@ -10,6 +10,10 @@ import {
   Tabs,
   Tab,
   LinearProgress,
+  Select,
+  InputLabel,
+  MenuItem,
+  FormControl,
 } from "@mui/material";
 import SparkIcon from "@mui/icons-material/OfflineBoltOutlined";
 import { useConfirmation } from "components/ConfirmationDialog";
@@ -18,22 +22,7 @@ import SnackbarProgress, {
   ISnackbarProgressRef,
 } from "components/SnackbarProgress";
 import { useProjectContext } from "@src/contexts/ProjectContext";
-
-const typographyVariants = [
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "subtitle1",
-  "subtitle2",
-  "body1",
-  "body2",
-  "button",
-  "caption",
-  "overline",
-];
+import { RunRoutes } from "@src/constants/runRoutes";
 
 export default function TestView() {
   const theme = useTheme();
@@ -44,9 +33,17 @@ export default function TestView() {
   const [result, setResult] = useState<any>({});
 
   const [method, setMethod] = useState<"GET" | "POST">("GET");
-  const [path, setPath] = useState("");
+  const [path, setPath] = useState<string>("/");
   const handleMethodChange = (_, newMethod) => setMethod(newMethod);
-
+  const setDefinedRoute = (newPath) => {
+    setPath(newPath.target.value);
+    const _method = Object.values(RunRoutes).find(
+      (r) => r.path === path
+    )?.method;
+    if (_method) {
+      setMethod(_method);
+    }
+  };
   const handleRun = async () => {
     console.log("run");
     if (!rowyRun) return;
@@ -64,6 +61,21 @@ export default function TestView() {
     <Navigation title="Rowy Run Sandbox">
       {loading && <LinearProgress />}
       <Container style={{ margin: "24px 0 200px" }}>
+        <FormControl style={{ minWidth: 240 }}>
+          <InputLabel>Defined Route</InputLabel>
+          <Select
+            value={
+              Object.values(RunRoutes).find((r) => r.path === path)?.path ?? ""
+            }
+            onChange={setDefinedRoute}
+          >
+            {Object.values(RunRoutes).map((route) => (
+              <MenuItem key={route.path} value={route.path}>
+                {route.path}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <Tabs value={method} onChange={handleMethodChange}>
           <Tab label="GET" value="GET" />
           <Tab label="POST" value="POST" />
@@ -71,7 +83,7 @@ export default function TestView() {
           <Tab label="DELETE" value="DELETE" />
         </Tabs>
         <TextField
-          defaultValue={"/"}
+          value={path}
           onChange={(value) => {
             setPath(value.target.value);
           }}
