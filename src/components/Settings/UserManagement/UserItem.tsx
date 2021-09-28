d5105daf602d4587cd00440b3f9bc2b2663dbc68
Sidney Alcantara <sidney@sidney.me>
2021-09-28 21:42:34 +1000

    UserItem: fix multiselect styles being overridden

--- src/components/Settings/UserManagement/UserItem.tsx
+++ src/components/Settings/UserManagement/UserItem.tsx
@@ -126,24 +126,24 @@ export default function UserItem({ id, user, roles: rolesProp }: User) {
               sx: {
                 mr: 0.5,
 
-                "& .MuiInputLabel-root": {
+                "&& .MuiInputLabel-root": {
                   opacity: 0,
                   mt: -3,
                 },
 
-                "& .MuiFilledInput-root": {
+                "&& .MuiFilledInput-root": {
                   bgcolor: "transparent",
                   boxShadow: 0,
                   "&::before": { content: "none" },
 
                   "&:hover, &.Mui-focused": { bgcolor: "action.hover" },
                 },
-                "& .MuiSelect-select.MuiFilledInput-input": {
+                "&& .MuiSelect-select.MuiFilledInput-input": {
                   typography: "button",
                   pl: 1,
                   pr: 3.25,
                 },
-                "& .MuiSelect-icon": {
+                "&& .MuiSelect-icon": {
                   right: 2,
                 },
               },
