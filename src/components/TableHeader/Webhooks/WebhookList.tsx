e731034c5e4117ebfc62150b376079e9268d65c9
shamsmosowi <shams.mosowi@gmail.com>
2021-11-25 14:39:53 +1100

    use cloudLogs atom

--- src/components/TableHeader/Webhooks/WebhookList.tsx
+++ src/components/TableHeader/Webhooks/WebhookList.tsx
@@ -28,6 +28,11 @@ import EmptyState from "@src/components/EmptyState";
 import { webhookTypes, webhookNames, IWebhook, WebhookType } from "./utils";
 import { DATE_TIME_FORMAT } from "@src/constants/dates";
 import { useProjectContext } from "@src/contexts/ProjectContext";
+import { useAtom } from "jotai";
+import {
+  modalAtom,
+  cloudLogFiltersAtom,
+} from "@src/components/TableHeader/CloudLogs/utils";
 
 export interface IWebhookListProps {
   webhooks: IWebhook[];
@@ -48,6 +53,8 @@ export default function WebhookList({
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const addButtonRef = useRef(null);
 
+  const [, setModal] = useAtom(modalAtom);
+  const [, setCloudLogFilters] = useAtom(cloudLogFiltersAtom);
   const activeWebhookCount = webhooks.filter(
     (webhook) => webhook.active
   ).length;
@@ -188,17 +195,19 @@ export default function WebhookList({
                     </Tooltip>
 
                     <Tooltip title="Logs">
-                      <Link
-                        component={IconButton}
+                      <IconButton
                         aria-label="Logs"
-                        href={`${
-                          window.location.pathname
-                        }#modal="cloudLogs"&cloudLogFilters=${encodeURIComponent(
-                          `{"type":"webhook","timeRange":{"type":"days","value":7},"webhook":["${webhook.endpoint}"]}`
-                        )}`}
+                        onClick={() => {
+                          setModal("cloudLogs");
+                          setCloudLogFilters({
+                            type: "webhook",
+                            timeRange: { type: "days", value: 7 },
+                            webhook: [webhook.endpoint],
+                          });
+                        }}
                       >
                         <LogsIcon />
-                      </Link>
+                      </IconButton>
                     </Tooltip>
                     <Tooltip title="Edit">
                       <IconButton
