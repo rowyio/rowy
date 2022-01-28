843d67241f33dad8cbe1912c63e85e2585ca8a9d
Sidney Alcantara <sidney@sidney.me>
2022-01-28 12:09:09 +1100

    add comment

--- src/components/TableSettings/index.tsx
+++ src/components/TableSettings/index.tsx
@@ -91,6 +91,7 @@ export default function TableSettings({
             }/${data.id}`;
 
             if (hasExtensions) {
+              // find derivative, default value
               snackLogContext.requestSnackLog();
               rowyRun({
                 route: runRoutes.buildFunction,
