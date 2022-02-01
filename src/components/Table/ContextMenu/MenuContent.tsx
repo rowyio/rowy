20fb3e9ca0e35e7b84d1b81732eb9a5953b0e64b
Gibson Han <gibsonthedev@gmail.com>
2022-02-01 17:41:24 +0700

    add context menu feature with cut, copy, and paste

---
+++ src/components/Table/ContextMenu/MenuContent.tsx
@@ -0,0 +1,42 @@
+import { Menu } from "@mui/material";
+import MenuRow, { IMenuRow } from "./MenuRow";
+
+interface IMenuContents {
+  anchorEl: HTMLElement;
+  open: boolean;
+  handleClose: () => void;
+  items: IMenuRow[];
+}
+
+export function MenuContents({
+  anchorEl,
+  open,
+  handleClose,
+  items,
+}: IMenuContents) {
+  const handleContext = (e: React.MouseEvent) => e.preventDefault();
+  return (
+    <Menu
+      id="cell-context-menu"
+      aria-labelledby="cell-context-menu"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+      sx={{ "& .MuiMenu-paper": { backgroundColor: "background.default" } }}
+      MenuListProps={{ disablePadding: true }}
+      onContextMenu={handleContext}
+    >
+      {items.map((item, indx: number) => (
+        <MenuRow key={indx} {...item} />
+      ))}
+    </Menu>
+  );
+}
--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -1,3 +1,4 @@
+import { useProjectContext } from "@src/contexts/ProjectContext";
 import { Fragment } from "react";
 import { Row, RowRendererProps } from "react-data-grid";
 
@@ -8,9 +9,27 @@ export default function TableRow(props: RowRendererProps<any>) {
     return (
       <Fragment key={props.row.id}>
         <OutOfOrderIndicator top={props.top} height={props.height} />
-        <Row {...props} />
+        <ContextMenu>
+          <Row {...props} />
+        </ContextMenu>
       </Fragment>
     );
 
-  return <Row {...props} />;
+  return (
+    <ContextMenu>
+      <Row {...props} />
+    </ContextMenu>
+  );
 }
+
+const ContextMenu = (props: any) => {
+  const { contextMenuRef }: any = useProjectContext();
+  function handleClick(e: any) {
+    e.preventDefault();
+    const input = e?.target as HTMLElement;
+    if (contextMenuRef?.current) {
+      contextMenuRef?.current?.setAnchorEl(input);
+    }
+  }
+  return <span onContextMenu={(e) => handleClick(e)}>{props.children}</span>;
+};
