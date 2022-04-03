269bf997de6fe3c070d4134353257137e40d4cc9
shamsmosowi <shams.mosowi@gmail.com>
2022-04-03 19:54:53 +0200

    connector

--- src/components/fields/Connector/Settings.tsx
+++ src/components/fields/Connector/Settings.tsx
@@ -62,120 +62,103 @@ const diagnosticsOptions = {
 export default function Settings({ config, onChange }) {
   const { projectId } = useAppContext();
   return (
-    <SteppedAccordion
-      steps={[
-        {
-          id: "function",
-          title: "Connector Function",
-          content: (
-            <Stack>
-              <Suspense fallback={<FieldSkeleton height={300} />}>
-                <CodeEditor
-                  minHeight={200}
-                  value={config.connectorFn ?? baseFunction}
-                  onChange={onChange("connectorFn")}
-                  diagnosticsOptions={diagnosticsOptions}
-                  extraLibs={[connectorDefs]}
-                />
-              </Suspense>
-              <CodeEditorHelper
-                docLink={WIKI_LINKS.fieldTypesAction + "#script"}
-                additionalVariables={[]}
-              />
-            </Stack>
-          ),
-        },
-        {
-          id: "config",
-          title: "Configuration",
-          content: (
-            <Stack spacing={3}>
-              <FormControl>
-                {/* <InputLabel variant="filled">Primary Key</InputLabel> */}
-                <TextField
-                  id="elementId"
-                  label="ID"
-                  placeholder="id"
-                  name="id"
-                  value={config.elementId}
-                  fullWidth
-                  onChange={(e) => onChange("elementId")(e.target.value)}
-                  helperText={
-                    <>
-                      The key that will be used to uniquely identify the
-                      selected option.{" "}
-                      <Link
-                        href={WIKI_LINKS.fieldTypesConnectService + "#id"}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Learn more
-                        <InlineOpenInNewIcon />
-                      </Link>
-                    </>
-                  }
-                />
-              </FormControl>
-              <FormControl>
-                {/* <InputLabel variant="filled">Title Key</InputLabel> */}
-                <TextField
-                  id="labelFormatter"
-                  label="Label Formatter"
-                  placeholder="{{id}} - {{name}}"
-                  name="labelFormatter"
-                  value={config.labelFormatter}
-                  fullWidth
-                  onChange={(e) => onChange("labelFormatter")(e.target.value)}
-                  helperText={
-                    <>
-                      The key that will be used to display the selected option.{" "}
-                      <Link
-                        href={
-                          WIKI_LINKS.fieldTypesConnectService +
-                          "#labelFormatter"
-                        }
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Learn more
-                        <InlineOpenInNewIcon />
-                      </Link>
-                    </>
-                  }
-                />
-              </FormControl>
-              <FormControl>
-                <Grid container>
-                  <InputLabel variant="filled">
-                    Allow for multiple item selection
-                  </InputLabel>
-                  <Switch
-                    checked={config.multiple}
-                    onChange={(e) => onChange("multiple")(e.target.checked)}
-                  />
-                </Grid>
-              </FormControl>
-              <FormControl>
-                {config.multiple && (
-                  <>
-                    <TextField
-                      type="number"
-                      id="max"
-                      label="Maximum"
-                      placeholder="Maximum"
-                      name="max"
-                      value={config.max}
-                      fullWidth
-                      onChange={(e) => onChange("max")(e.target.value)}
-                      helperText="The maximum number of items that can be selected, or 0 for no limit."
-                    />
-                  </>
-                )}
-              </FormControl>
-            </Stack>
-          ),
-        },
-      ]}
-    />
+    <>
+      <div>
+        <Typography variant="subtitle2">Connector Function</Typography>
+        <Suspense fallback={<FieldSkeleton height={200} />}>
+          <CodeEditor
+            minHeight={200}
+            value={config.connectorFn ?? baseFunction}
+            onChange={onChange("connectorFn")}
+            diagnosticsOptions={diagnosticsOptions}
+            extraLibs={[connectorDefs]}
+          />
+        </Suspense>
+        <CodeEditorHelper
+          docLink={WIKI_LINKS.fieldTypesConnector + "#examples"}
+          additionalVariables={[]}
+        />
+      </div>
+      <FormControl>
+        {/* <InputLabel variant="filled">Primary Key</InputLabel> */}
+        <TextField
+          id="elementId"
+          label="ID"
+          placeholder="id"
+          name="id"
+          value={config.elementId}
+          fullWidth
+          onChange={(e) => onChange("elementId")(e.target.value)}
+          helperText={
+            <>
+              The key that will be used to uniquely identify the selected
+              option.{" "}
+              <Link
+                href={WIKI_LINKS.fieldTypesConnector + "#api"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn more
+                <InlineOpenInNewIcon />
+              </Link>
+            </>
+          }
+        />
+      </FormControl>
+      <FormControl>
+        {/* <InputLabel variant="filled">Title Key</InputLabel> */}
+        <TextField
+          id="labelFormatter"
+          label="Label Formatter"
+          placeholder="{{id}} - {{name}}"
+          name="labelFormatter"
+          value={config.labelFormatter}
+          fullWidth
+          onChange={(e) => onChange("labelFormatter")(e.target.value)}
+          helperText={
+            <>
+              The field key or template that will be used to display the
+              selected option.{" "}
+              <Link
+                href={WIKI_LINKS.fieldTypesConnector + "#api"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn more
+                <InlineOpenInNewIcon />
+              </Link>
+            </>
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <Grid container>
+          <InputLabel variant="filled">
+            Allow for multiple item selection
+          </InputLabel>
+          <Switch
+            checked={config.multiple}
+            onChange={(e) => onChange("multiple")(e.target.checked)}
+          />
+        </Grid>
+      </FormControl>
+      <FormControl>
+        {config.multiple && (
+          <>
+            <TextField
+              type="number"
+              id="max"
+              label="Maximum"
+              placeholder="Maximum"
+              name="max"
+              value={config.max}
+              fullWidth
+              onChange={(e) => onChange("max")(e.target.value)}
+              helperText="The maximum number of items that can be selected, or 0 for no limit."
+            />
+          </>
+        )}
+      </FormControl>
+    </>
   );
 }
