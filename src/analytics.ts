94372bc2f6112d9b3040664c08f66cb25c19066b
Sidney Alcantara <sidney@sidney.me>
2022-06-06 13:08:50 +1000

    disable analytics on rowy open source

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,16 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics, logEvent } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyArABiYGK7dZgwSk0pw_6vKbOt6U1ZRPpc",
-  authDomain: "rowy-service.firebaseapp.com",
-  projectId: "rowy-service",
-  storageBucket: "rowy-service.appspot.com",
-  messagingSenderId: "305614947641",
-  appId: "1:305614947641:web:cb10467e7c11c93d6e14e8",
-  measurementId: "G-0VWE25LFZJ",
-};
-
-const rowyServiceApp = initializeApp(firebaseConfig, "rowy-service");
-export const analytics = getAnalytics(rowyServiceApp);
-export { logEvent };
+/**
+ * Analytics is disabled on Rowy open source.
+ */
+import type { logEvent as logEventFunction } from "firebase/analytics";
+export const analytics = {} as any;
+export const logEvent = (() => {}) as typeof logEventFunction;
