f8c426976797c46ed2c931b8e241015abbded0f3
Han Tuerker <burhan.tuerker@gmail.com>
2022-06-22 22:25:50 +0300

    feat(percentage-c11n): add color picker to settings

---
+++ src/components/ColorPickerInput.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { ButtonBase, Box, Collapse } from "@mui/material";
+
+import { fieldSx } from "@src/components/SideDrawer/utils";
+import { ChevronDown } from "@src/assets/icons/ChevronDown";
+import { Color, ColorPicker } from "react-color-palette";
+
+export interface IColorPickerProps {
+  value: Color;
+  handleChange: (color: Color) => void;
+  handleOnChangeComplete: (color: Color) => void;
+}
+
+export default function ColorPickerInput({
+  value,
+  handleChange,
+  handleOnChangeComplete,
+}: IColorPickerProps) {
+  const [showPicker, setShowPicker] = useState(false);
+  const toggleOpen = () => setShowPicker((s) => !s);
+  return (
+    <>
+      <ButtonBase
+        onClick={toggleOpen}
+        component={ButtonBase}
+        focusRipple
+        sx={[
+          fieldSx,
+          {
+            justifyContent: "flex-start",
+            "&&": { pl: 0.75, pr: 0.5 },
+            color: "red",
+            transition: (theme) =>
+              theme.transitions.create("border-radius", {
+                delay: theme.transitions.duration.standard,
+              }),
+            "&.Mui-disabled": { color: "text.disabled" },
+          },
+          showPicker && {
+            transitionDelay: "0s",
+            transitionDuration: "0s",
+            borderBottomLeftRadius: 0,
+            borderBottomRightRadius: 0,
+          },
+        ]}
+      >
+        <Box
+          sx={{
+            backgroundColor: value.hex,
+            width: 15,
+            height: 15,
+            mr: 1.5,
+            boxShadow: (theme) => `0 0 0 1px ${theme.palette.divider} inset`,
+            borderRadius: 0.5,
+          }}
+        />
+
+        <div style={{ flexGrow: 1 }}>{value.hex}</div>
+
+        <ChevronDown
+          color="action"
+          sx={{
+            transition: (theme) => theme.transitions.create("transform"),
+            transform: showPicker ? "rotate(180deg)" : "none",
+          }}
+        />
+      </ButtonBase>
+
+      <Collapse
+        in={showPicker}
+        sx={{
+          "& .rcp": {
+            borderTop: 0,
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0,
+            "& .rcp-saturation": {
+              borderRadius: 0,
+            },
+          },
+        }}
+      >
+        <ColorPicker
+          width={400}
+          height={100}
+          color={value}
+          onChange={handleChange}
+          onChangeComplete={handleOnChangeComplete}
+          alpha
+        />
+      </Collapse>
+    </>
+  );
+}
--- src/components/fields/Percentage/Settings.tsx
+++ src/components/fields/Percentage/Settings.tsx
@@ -1,10 +1,52 @@
+import ColorPickerInput from "@src/components/ColorPickerInput";
 import { ISettingsProps } from "@src/components/fields/types";
 
+import { useState } from "react";
+import { Color, toColor } from "react-color-palette";
+
 export default function Settings({ onChange, config }: ISettingsProps) {
+  const defaultColors = {
+    startColor: toColor("hex", "#f00"),
+    midColor: toColor("hex", "#ff0"),
+    endColor: toColor("hex", "#0f0"),
+  };
+
+  const {
+    startColor,
+    midColor,
+    endColor,
+  }: {
+    startColor: Color;
+    endColor: Color;
+    midColor: Color;
+  } = {
+    ...defaultColors,
+    ...config,
+  };
+
+  console.log(startColor, midColor, endColor);
+
+  const [start, setStart] = useState<Color>(startColor);
+  const [mid, setMid] = useState<Color>(midColor);
+  const [end, setEnd] = useState<Color>(endColor);
   return (
     <>
       {`Config: ${JSON.stringify(config)}`}
-      <button onClick={() => onChange("foo")("bar")} />
+      <ColorPickerInput
+        value={start}
+        handleChange={(color) => setStart(color)}
+        handleOnChangeComplete={(color) => onChange("startColor")(color)}
+      />
+      <ColorPickerInput
+        value={mid}
+        handleChange={(color) => setMid(color)}
+        handleOnChangeComplete={(color) => onChange("midColor")(color)}
+      />
+      <ColorPickerInput
+        value={end}
+        handleChange={(color) => setEnd(color)}
+        handleOnChangeComplete={(color) => onChange("endColor")(color)}
+      />
     </>
   );
 }
