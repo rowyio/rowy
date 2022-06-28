126f035b63f19b0bf2560554054f0f57dd7dc14d
Han Tuerker <burhan.tuerker@gmail.com>
2022-06-28 22:27:51 +0300

    fix(rich-text-editor): add stylings to dropdown

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-
-import { styled, useTheme } from "@mui/material";
+import { GlobalStyles } from "tss-react";
+import { alpha, styled, useTheme } from "@mui/material";
 import { Editor } from "@tinymce/tinymce-react";
 
 // TinyMCE so the global var exists
@@ -120,51 +120,82 @@ export default function RichTextEditor({
 
   return (
     <Styles focus={focus} disabled={disabled}>
-    <style>{theme.palette.mode === "dark" ? skinDarkCss : skinCss}</style>
-        <GlobalStyles
-          styles={{
-            ".tox": {
-              "& .tox-menu.tox-menu, &.tox-tinymce-aux .tox-toolbar__overflow.tox-toolbar__overflow":
-                {
-                  backgroundColor: theme.palette.background.paper,
-                  backgroundImage:
-                    "linear-gradient(rgba(255, 255, 255, 0.12), rgba(255, 255, 255, 0.12))", // elevation 8
-                  boxShadow: theme.shadows[8],
-                  border: "none",
-                  borderRadius: (theme.shape.borderRadius as number) * 2,
-                },
+      <style>{theme.palette.mode === "dark" ? skinDarkCss : skinCss}</style>
+      <GlobalStyles
+        styles={{
+          ".tox": {
+            "& .tox-menu.tox-menu, &.tox-tinymce-aux .tox-toolbar__overflow.tox-toolbar__overflow":
+              {
+                backgroundColor: theme.palette.background.paper,
+                backgroundImage:
+                  "linear-gradient(rgba(255, 255, 255, 0.12), rgba(255, 255, 255, 0.12))", // elevation 8
+                boxShadow: theme.shadows[8],
+                border: "none",
+                borderRadius: (theme.shape.borderRadius as number) * 2,
+              },
 
-              "& .tox-collection--list": {
-                "& .tox-collection__group.tox-collection__group": {
-                  padding: theme.spacing(0.5),
-                },
-                "& .tox-collection__item": {
+            "& .tox-collection--list": {
+              "& .tox-collection__group.tox-collection__group": {
+                padding: theme.spacing(0.5),
+                paddingLeft: 0,
+                paddingRight: 0,
+              },
+              "& .tox-collection__item": {
+                padding: theme.spacing(0.5),
+                marginLeft: theme.spacing(0.5),
+                marginRight: theme.spacing(0.5),
+                borderRadius: theme.shape.borderRadius,
+              },
+
+              "& .tox-collection__item--enabled": {
+                backgroundColor: theme.palette.action.hover + " !important",
+                position: "relative",
+                "&::before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: theme.spacing(1),
+                  bottom: theme.spacing(1),
+                  left: 0,
+                  width: theme.spacing(0.3),
                   borderRadius: theme.shape.borderRadius,
+                  backgroundColor: theme.palette.primary.main,
                 },
               },
 
-              "&.tox-tinymce-aux .tox-toolbar__overflow.tox-toolbar__overflow":
-                {
-                  padding: theme.spacing(0.5, 0),
-                },
-              "& .tox-tbtn.tox-tbtn": {
-                borderRadius: theme.shape.borderRadius,
-                margin: 0,
+              "& .tox-collection__item--active": {
+                backgroundColor:
+                  alpha(
+                    theme.palette.primary.main,
+                    theme.palette.action.selectedOpacity
+                  ) + "!important",
+              },
+              "& .tox-collection__item-checkmark": {
+                display: "none",
               },
             },
-          }}
-        />
-        
-        <Editor
-          key={theme.palette.mode}
-          disabled={disabled}
-          init={{
-            skin: false,
-            content_css: false,
-            content_style: [
-              theme.palette.mode === "dark" ? contentCssDark : contentCss,
-              theme.palette.mode === "dark" ? contentUiCssDark : contentUiCss,
-              `
+
+            "&.tox-tinymce-aux .tox-toolbar__overflow.tox-toolbar__overflow": {
+              padding: theme.spacing(0.5, 0),
+            },
+            "& .tox-tbtn.tox-tbtn": {
+              borderRadius: theme.shape.borderRadius,
+              margin: 0,
+            },
+          },
+        }}
+      />
+
+      <Editor
+        key={theme.palette.mode}
+        disabled={disabled}
+        init={{
+          skin: false,
+          content_css: false,
+          content_style: [
+            theme.palette.mode === "dark" ? contentCssDark : contentCss,
+            theme.palette.mode === "dark" ? contentUiCssDark : contentUiCss,
+            `
               :root {
                 font-size: 14px;
                 -webkit-font-smoothing: antialiased;
@@ -183,26 +214,26 @@ export default function RichTextEditor({
               body :first-child { margin-top: 0; }
               a { color: ${theme.palette.primary.main}; }
             `,
-            ].join("\n"),
-            minHeight: 300,
-            menubar: false,
-            plugins: ["autoresize", "lists link image", "paste help", "code"],
-            statusbar: false,
-            toolbar:
-              "formatselect | bold italic forecolor | link | bullist numlist outdent indent | removeformat code | help",
-            body_id: id,
-          }}
-          value={value}
-          onEditorChange={onChange}
-          onFocus={() => {
-            setFocus(true);
-            if (onFocus) onFocus();
-          }}
-          onBlur={() => {
-            setFocus(false);
-            if (onBlur) onBlur();
-          }}
-        />
+          ].join("\n"),
+          minHeight: 300,
+          menubar: false,
+          plugins: ["autoresize", "lists link image", "paste help", "code"],
+          statusbar: false,
+          toolbar:
+            "formatselect | bold italic forecolor | link | bullist numlist outdent indent | removeformat code | help",
+          body_id: id,
+        }}
+        value={value}
+        onEditorChange={onChange}
+        onFocus={() => {
+          setFocus(true);
+          if (onFocus) onFocus();
+        }}
+        onBlur={() => {
+          setFocus(false);
+          if (onBlur) onBlur();
+        }}
+      />
     </Styles>
   );
 }
