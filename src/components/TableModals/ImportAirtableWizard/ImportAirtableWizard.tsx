9adf1c257243fa1dcedf09b2c1137138784dd077
Han Tuerker <burhan.tuerker@gmail.com>
2022-08-09 03:07:39 +0300

    feat(airtable-migration: improve fetcher logic

--- src/components/TableModals/ImportAirtableWizard/ImportAirtableWizard.tsx
+++ src/components/TableModals/ImportAirtableWizard/ImportAirtableWizard.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef, useMemo } from "react";
+import { useState, useCallback, useRef } from "react";
 import useMemoValue from "use-memo-value";
 import { useAtom, useSetAtom } from "jotai";
 import { RESET } from "jotai/utils";
@@ -30,7 +30,6 @@ import {
   tableModalAtom,
 } from "@src/atoms/tableScope";
 import { ColumnConfig } from "@src/types/table";
-import { getFieldProp } from "@src/components/fields";
 import Step2NewColumns from "./Step2NewColumns";
 import SnackbarProgress, {
   ISnackbarProgressRef,
@@ -64,7 +63,7 @@ export default function ImportAirtableWizard({ onClose }: ITableModalProps) {
   const theme = useTheme();
   const isXs = useMediaQuery(theme.breakpoints.down("sm"));
   const snackbarProgressRef = useRef<ISnackbarProgressRef>();
-
+  const countRef = useRef(0);
   const columns = useMemoValue(tableSchema.columns ?? {}, isEqual);
 
   const [config, setConfig] = useState<AirtableConfig>({
@@ -87,12 +86,14 @@ export default function ImportAirtableWizard({ onClose }: ITableModalProps) {
     const url = offset
       ? `https://api.airtable.com/v0/${baseId}/${tableId}?offset=${offset}`
       : `https://api.airtable.com/v0/${baseId}/${tableId}`;
+
     const response = await fetch(url, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${apiKey}`,
       },
     }).then((response) => response.json());
+
     return response;
   };
 
@@ -101,12 +102,12 @@ export default function ImportAirtableWizard({ onClose }: ITableModalProps) {
       case FieldType.multiSelect:
         return (v: string[]) => v;
       default:
-        return getFieldProp("csvImportParser", fieldType);
+        return null;
     }
   };
 
-  const parsedRows = (currentBatch: any[]): any[] =>
-    currentBatch.map((record) =>
+  const parsedRecords = (records: any[]): any[] =>
+    records.map((record) =>
       config.pairs.reduce((a, pair) => {
         const matchingColumn =
           columns[pair.columnKey] ??
@@ -123,14 +124,14 @@ export default function ImportAirtableWizard({ onClose }: ITableModalProps) {
     console.time("importAirtable");
     snackbarProgressRef.current?.setProgress(0);
     const loadingSnackbar = enqueueSnackbar(
-      `Importing rows. This might take a while.`,
+      `Importing records. This might take a while.`,
       {
         persist: true,
         action: (
           <SnackbarProgress
             stateRef={snackbarProgressRef}
             target={0}
-            label=" rows"
+            label=" records"
           />
         ),
       }
@@ -154,53 +155,64 @@ export default function ImportAirtableWizard({ onClose }: ITableModalProps) {
     );
 
     try {
-      let currentBatch: any[] = [];
-      const fetcher = async (i: number, offset: string) => {
-        // Airtable Rate Limits: 5 req/sec , 100rows/req
-        if (!offset) return;
-        const { records, offset: currentOffset } = await fetchRecords(offset);
-        currentBatch = [...currentBatch, ...records];
-        snackbarProgressRef.current?.setTarget((prev) => prev + records.length);
-        if (i < 5) {
-          fetcher(++i, currentOffset);
-        } else {
-          setTimeout(() => {
+      const promises: Promise<void>[] = [];
+      const timeout = (ms: number): Promise<void> =>
+        new Promise((resolve) => setTimeout(() => resolve(), ms));
+
+      // Airtable Rate Limits: 5 req/sec
+      const RATE_LIMIT = { REQ_PER_SECOND: 5 };
+      const fetcher = async (i: number, offset?: string): Promise<void> => {
+        console.log(i, offset, promises);
+        if (offset) {
+          const { records, offset: nextPage } = await fetchRecords(offset);
+          snackbarProgressRef.current?.setTarget(
+            (prev) => prev + records.length
+          );
+          promises.push(
             bulkAddRows({
-              rows: parsedRows(currentBatch),
+              rows: parsedRecords(records),
               collection: tableSettings.collection,
-              onBatchCommit: () =>
-                snackbarProgressRef.current?.setProgress(
-                  (prev) => prev + currentBatch.length
-                ),
-            });
-            currentBatch = [];
-            fetcher(0, currentOffset);
-          }, 1050);
+            }).then(() => {
+              countRef.current += records.length;
+              snackbarProgressRef.current?.setProgress(
+                (prev) => prev + records.length
+              );
+            })
+          );
+          if (i < RATE_LIMIT.REQ_PER_SECOND - 1) {
+            promises.push(fetcher(++i, nextPage));
+          } else {
+            promises.push(timeout(1050).then(() => fetcher(0, nextPage)));
+          }
         }
+      };
+      const recursiveAll = async (): Promise<void[]> => {
+        return Promise.all(promises).then((result) => {
+          if (result.length === promises.length) {
+            return result;
+          }
+          return recursiveAll();
+        });
       };
 
       onClose();
 
-      const promises: Promise<void>[] = [];
       for (const col of config.newColumns)
         promises.push(addColumn({ config: col }));
 
       await Promise.all(promises);
 
-      const { records, offset } = await fetchRecords();
-      currentBatch = records;
-      snackbarProgressRef.current?.setTarget(currentBatch.length);
-      await fetcher(1, offset);
-      if (currentBatch.length > 0) {
-        await bulkAddRows({
-          rows: parsedRows(currentBatch),
-          collection: tableSettings.collection,
-          onBatchCommit: () =>
-            snackbarProgressRef.current?.setProgress(
-              (prev) => prev + currentBatch.length
-            ),
-        });
-      }
+      const { records, offset: nextPage } = await fetchRecords();
+      snackbarProgressRef.current?.setTarget(records.length);
+      countRef.current += records.length;
+      console.log("before fetcher");
+      await fetcher(1, nextPage);
+      await recursiveAll();
+      enqueueSnackbar(
+        `Imported ${Number(countRef.current).toLocaleString()} rows`,
+        { variant: "success" }
+      );
+      console.log("after fetcher");
     } catch (e) {
       enqueueSnackbar((e as Error).message, { variant: "error" });
     } finally {
