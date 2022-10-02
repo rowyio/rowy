bba260fb1c66d96147d13dcc26bb87c42950c703
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2022-10-02 21:47:43 +0530

    Bug:LongText working

--- src/components/fields/LongText/index.tsx
+++ src/components/fields/LongText/index.tsx
@@ -5,7 +5,8 @@ import withBasicCell from "@src/components/fields/_withTableCell/withBasicCell";
 import LongTextIcon from "@mui/icons-material/Notes";
 import BasicCell from "./BasicCell";
 import TextEditor from "@src/components/Table/editors/TextEditor";
-import { filterOperators } from "@src/components/fields/ShortText/Filter";
+
+import { filterOperators } from "./Filter";
 import BasicContextMenuActions from "@src/components/fields/_BasicCell/BasicCellContextMenuActions";
 
 const SideDrawerField = lazy(
@@ -16,7 +17,7 @@ const SideDrawerField = lazy(
 );
 
 const Settings = lazy(
-  () => import("./Settings" /* webpackChunkName: "Settings-ShortText" */)
+  () => import("./Settings" /* webpackChunkName: "Settings-LongText" */)
 );
 
 export const config: IFieldConfig = {
