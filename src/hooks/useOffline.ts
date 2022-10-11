bebde59147d807b272e3a184c4a46050422f1dad
Sidney Alcantara <sidney@sidney.me>
2022-10-12 10:45:41 +1100

    fix useOffline having incorrect state when listener isn’t fired

--- src/hooks/useOffline.ts
+++ src/hooks/useOffline.ts
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 
 export default function useOffline() {
-  const [isOffline, setIsOffline] = useState(true);
+  const [isOffline, setIsOffline] = useState(false);
   const handleOffline = () => setIsOffline(true);
   const handleOnline = () => setIsOffline(false);
 
   useEffect(() => {
+    // Need to set here because the listener doesn’t fire on initial load
+    setIsOffline(!window.navigator.onLine);
+
     window.addEventListener("offline", handleOffline);
     window.addEventListener("online", handleOnline);
 
