8e899dad7802b676ab57212cf16ddbe96057c546
Bobby Wang <bofei@mail.com>
2022-11-22 03:04:26 +0800

    enable derivatives logging in code editor

--- src/components/fields/Derivative/derivative.d.ts
+++ src/components/fields/Derivative/derivative.d.ts
@@ -5,6 +5,11 @@ type DerivativeContext = {
   db: FirebaseFirestore.Firestore;
   auth: firebaseauth.BaseAuth;
   change: any;
+  logging: {
+    log: (payload: any) => void;
+    warn: (payload: any) => void;
+    error: (payload: any) => void;
+  };
 };
 
 type Derivative = (context: DerivativeContext) => "PLACEHOLDER_OUTPUT_TYPE";
