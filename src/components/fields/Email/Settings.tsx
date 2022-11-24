40dcf4f60f751b282024e39c41b96c2f8a406c21
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2022-11-24 22:34:12 +0530

    ref added with button and inputfield but current not working
    Signed-off-by: Raj Gaurav Maurya <rajgmsocial19@gmail.com>

--- src/components/fields/Email/Settings.tsx
+++ src/components/fields/Email/Settings.tsx
@@ -1,21 +1,30 @@
 import { ISettingsProps } from "@src/components/fields/types";
-import { TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import { useRef } from "react";
 
 export default function Settings({ onChange, config }: ISettingsProps) {
+
+    const emailField = useRef<HTMLInputElement>(null);
+    const useStandardRegex=()=>{
+        emailField.current.value="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-z]{2,3}$";
+    }
+
     return (
         <>
             <TextField
-                type="text"
+                type="text" ref={emailField}
                 label="Validation regex"
                 id="validation-regex"
                 value={config.validationRegex}
-                defaultValue="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-z]{2,3}$"
                 fullWidth
                 onChange={(e) => {
                     if (e.target.value === "") onChange("validationRegex")(null);
                     else onChange("validationRegex")(e.target.value);
                 }}
             />
+            <Button style={{width:"200px", margin:"20px auto auto"}} onClick={useStandardRegex}>
+                Use standard regex
+            </Button>
         </>
     );
 }
