991e7630b6c093aeaf3c1c2cea7433d1d0b3f4c7
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2022-11-28 21:50:45 +0530

    Feat: Copy feature added, ref deleted, working as expected
    Signed-off-by: Raj Gaurav Maurya <rajgmsocial19@gmail.com>

--- src/components/fields/Email/Settings.tsx
+++ src/components/fields/Email/Settings.tsx
@@ -1,20 +1,15 @@
 import { ISettingsProps } from "@src/components/fields/types";
 import { TextField, Button } from "@mui/material";
-import { useRef } from "react";
 
 export default function Settings({ onChange, config }: ISettingsProps) {
 
-    const emailFieldValidation = useRef<HTMLInputElement>(null);
-    const useStandardRegex = () => {
-        if (emailFieldValidation.current != null) {
-            emailFieldValidation.current.value = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-z]{2,3}$";  
-        }
+    const copyStandardRegex = () => {
+      navigator.clipboard.writeText('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-zA-z]{2,3}$');
     }
 
     return (
         <>
             <TextField
-                inputRef={emailFieldValidation}
                 type="text"
                 label="Validation regex"
                 id="validation-regex"
@@ -25,8 +20,8 @@ export default function Settings({ onChange, config }: ISettingsProps) {
                     else onChange("validationRegex")(e.target.value);
                 }}
             />
-            <Button style={{ width: "200px", margin: "20px auto auto" }} onClick={useStandardRegex}>
-                Use standard Regex
+            <Button style={{ width: "200px", margin: "20px auto auto" }} onClick={copyStandardRegex}>
+                Copy standard Regex to clipboard
             </Button>
         </>
     );
