77beb429c4233dcb340afc02b82c4cc349194ca1
saravanan <saravananar001@gmail.com>
2022-12-13 12:59:46 +0530

    Feat: add reordering of Select and Multiselect field option

--- src/components/fields/SingleSelect/Settings.tsx
+++ src/components/fields/SingleSelect/Settings.tsx
@@ -15,6 +15,78 @@ import {
 import AddIcon from "@mui/icons-material/AddCircle";
 import RemoveIcon from "@mui/icons-material/CancelRounded";
 
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import { useDrag, useDrop } from "react-dnd";
+
+interface IsettingsDraggableProps {
+  options: string[];
+  option: string;
+  index: number;
+  onChange: (key: string) => (value: any) => void;
+}
+
+function DraggableSettingsCard({
+  options,
+  option,
+  index,
+  onChange,
+}: IsettingsDraggableProps) {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "SETTING_DRAG",
+    item: { key: index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const [{ isOver }, dropRef] = useDrop({
+    accept: "SETTING_DRAG",
+    drop: ({ key }: { key: number }) => {
+      const temp = options[key];
+      options[key] = options[index];
+      options[index] = temp;
+      onChange("options")([...options]);
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  return (
+    <div key={index}>
+      <Grid
+        ref={(ref) => {
+          dragRef(ref);
+          dropRef(ref);
+        }}
+        style={{ cursor: "move" }}
+        container
+        direction="row"
+        key={`option-${option}`}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item>
+          <Typography>{option}</Typography>
+        </Grid>
+        <Grid item>
+          <IconButton
+            aria-label="Remove"
+            onClick={() =>
+              onChange("options")(options.filter((o: string) => o !== option))
+            }
+          >
+            {<RemoveIcon />}
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Divider />
+    </div>
+  );
+}
+
 export default function Settings({ onChange, config }: ISettingsProps) {
   const listEndRef: any = useRef(null);
   const options = config.options ?? [];
@@ -42,7 +114,18 @@ export default function Settings({ onChange, config }: ISettingsProps) {
           marginBottom: 5,
         }}
       >
-        {options?.map((option: string) => (
+        <DndProvider backend={HTML5Backend}>
+          {options?.map((option: string, index: number) => (
+            <DraggableSettingsCard
+              options={options}
+              option={option}
+              index={index}
+              onChange={onChange}
+            />
+          ))}
+        </DndProvider>
+
+        {/* {options?.map((option: string) => (
           <>
             <Grid
               container
@@ -69,7 +152,7 @@ export default function Settings({ onChange, config }: ISettingsProps) {
             </Grid>
             <Divider />
           </>
-        ))}
+        ))} */}
         <div ref={listEndRef} style={{ height: 40 }} />
       </div>
 
@@ -93,7 +176,7 @@ export default function Settings({ onChange, config }: ISettingsProps) {
             onChange={(e) => {
               setNewOption(e.target.value);
             }}
-            onKeyDown={(e: any) => {
+            onKeyPress={(e: any) => {
               if (e.key === "Enter") {
                 handleAdd();
               }
