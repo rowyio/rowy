4bde3ce1721f152e45ea135923a1911f26fa5536
Han Tuerker <burhan.tuerker@gmail.com>
2022-12-18 20:27:41 +0800

    fix empty formula function

--- src/components/fields/Formula/DisplayCell.tsx
+++ src/components/fields/Formula/DisplayCell.tsx
@@ -6,7 +6,7 @@ export default function Formula(props: IDisplayCellProps) {
   const { result, error } = useFormula({
     row: props.row,
     listenerFields: props.column.config?.listenerFields || [],
-    formulaFn: props.column.config?.formulaFn,
+    formulaFn: props.column.config?.formulaFn || "",
   });
 
   const type = props.column.config?.renderFieldType;
