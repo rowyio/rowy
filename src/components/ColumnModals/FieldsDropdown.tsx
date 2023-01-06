f7cce06320112a9dd4145eeca934956833c774c2
Bobby Wang <bofei@mail.com>
2023-01-07 04:20:16 +1300

    requires cloud function setup before creating related columns

--- src/components/ColumnModals/FieldsDropdown.tsx
+++ src/components/ColumnModals/FieldsDropdown.tsx
@@ -5,12 +5,20 @@ import { FIELDS } from "@src/components/fields";
 import { FieldType } from "@src/constants/fields";
 import { getFieldProp } from "@src/components/fields";
 
+import { useSetAtom, useAtom } from "jotai";
+import {
+  projectScope,
+  projectSettingsAtom,
+  rowyRunModalAtom,
+} from "@src/atoms/projectScope";
+
 export interface IFieldsDropdownProps {
   value: FieldType | "";
   onChange: (value: FieldType) => void;
   hideLabel?: boolean;
   label?: string;
   options?: FieldType[];
+
   [key: string]: any;
 }
 
@@ -25,13 +33,21 @@ export default function FieldsDropdown({
   options: optionsProp,
   ...props
 }: IFieldsDropdownProps) {
+  const [projectSettings] = useAtom(projectSettingsAtom, projectScope);
+  const openRowyRunModal = useSetAtom(rowyRunModalAtom, projectScope);
   const fieldTypesToDisplay = optionsProp
     ? FIELDS.filter((fieldConfig) => optionsProp.indexOf(fieldConfig.type) > -1)
     : FIELDS;
-  const options = fieldTypesToDisplay.map((fieldConfig) => ({
-    label: fieldConfig.name,
-    value: fieldConfig.type,
-  }));
+  const options = fieldTypesToDisplay.map((fieldConfig) => {
+    const requireCloudFunctionSetup =
+      fieldConfig.requireCloudFunction && !projectSettings.rowyRunUrl;
+    return {
+      label: fieldConfig.name,
+      value: fieldConfig.type,
+      disabled: requireCloudFunctionSetup,
+      requireCloudFunctionSetup,
+    };
+  });
 
   return (
     <MultiSelect
@@ -51,7 +67,27 @@ export default function FieldsDropdown({
           <ListItemIcon style={{ minWidth: 40 }}>
             {getFieldProp("icon", option.value as FieldType)}
           </ListItemIcon>
-          {option.label}
+          {option.label}{" "}
+          {option.requireCloudFunctionSetup && (
+            <>
+              (requires
+              <span
+                style={{
+                  marginLeft: "3px",
+                  cursor: "pointer",
+                  pointerEvents: "all",
+                  textDecoration: "underline",
+                }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  openRowyRunModal({ feature: option.label });
+                }}
+              >
+                Cloud Function
+              </span>
+              )
+            </>
+          )}
         </>
       )}
       label={label || "Field type"}
